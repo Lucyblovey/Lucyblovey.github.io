307ed699539eea00c3923dbb23b393cc53fbb9af
aiyc <1432803776@qq.com>
2022-05-26 19:49:49 +0800

    update

--- lucyblovey/.vuepress/theme.ts
+++ lucyblovey/.vuepress/theme.ts
@@ -31,7 +31,7 @@ export default hopeTheme({
   pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
 
   blog: {
-    description: "一个前端开发者",
+    description: "文字是有灵魂的",
     intro: "/intro.html",
     medias: {
       // Baidu: "https://example.com",
@@ -77,9 +77,6 @@ export default hopeTheme({
       autoExcerpt: true,
     },
 
-    // 如果你不需要评论，可以直接删除 comment 配置，
-    // 以下配置仅供体验，如果你需要评论，请自行配置并使用自己的环境，详见文档。
-    // 为了避免打扰主题开发者以及消耗他的资源，请不要在你的正式环境中直接使用下列配置!!!!!
     comment: {
       /**
        * Using giscus
